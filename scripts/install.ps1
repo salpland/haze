802f12a52de8ad29e0f81c6dd44b14bdc07c3d6b
arexon <arexonreal@gmail.com>
2022-11-18 05:28:42 +0200

    chore: add installation script

---
+++ scripts/install.ps1
@@ -0,0 +1,26 @@
+#!/usr/bin/env pwsh
+
+$ErrorActionPreference = 'Stop'
+
+$DownloadUrl = "https://github.com/sedgeland/haze/releases/latest/download/haze-x86_64-pc-windows-msvc.zip"
+$BinDir = "${Home}\.haze"
+$HazeZip = "${BinDir}\haze.zip"
+$HazeExe = "${BinDir}\haze.exe"
+
+if (!(Test-Path $BinDir)) {
+  New-Item $BinDir -ItemType Directory | Out-Null
+}
+
+curl.exe -Lo $HazeZip $DownloadUrl
+tar.exe xf $HazeZip -C $BinDir
+Remove-Item $HazeZip
+
+$User = [System.EnvironmentVariableTarget]::User
+$Path = [System.Environment]::GetEnvironmentVariable('Path', $User)
+if (!(";${Path};".ToLower() -like "*;${BinDir};*".ToLower())) {
+  [System.Environment]::SetEnvironmentVariable('Path', "${Path};${BinDir}", $User)
+  $Env:Path += ";${BinDir}"
+}
+
+Write-Output "Haze was installed successfully to ${HazeExe}"
+Write-Output "Run 'haze --help' to get started"
