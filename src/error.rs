86b544f877a27ddd851bcf7c456910e873fbac31
arexon <arexonreal@gmail.com>
2022-11-22 03:39:01 +0200

    refactor: remove diagnostic codes

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 use colored::ColoredString;
 use miette::Diagnostic;
-use std::{fmt, io, path::PathBuf, vec};
+use std::{io, path::PathBuf};
 use thiserror::Error;
 
 pub type HazeResult<T> = Result<T, HazeError>;
@@ -8,11 +8,11 @@ pub type HazeResult<T> = Result<T, HazeError>;
 #[derive(Error, Diagnostic, Debug)]
 pub enum HazeError {
     #[error("couldn't read config file at `{1}`")]
-    #[diagnostic(code(haze::config::io_error), help("{0}"))]
+    #[diagnostic(help("{0}"))]
     ConfigRead(io::Error, ColoredString),
 
     #[error("config file at `{1}` was not parsed")]
-    #[diagnostic(code(haze::config::parse_error), help("{0}"))]
+    #[diagnostic(help("{0}"))]
     ConfigParse(serde_json::Error, ColoredString),
 
     #[error("the `worlds` config property is empty")]
@@ -24,7 +24,7 @@ pub enum HazeError {
     InvalidWorldsGlobPattern(glob::PatternError, String),
 
     #[error("cannot find a worlds directory from the defined patterns `{1:?}`")]
-    #[diagnostic(code(haze::config::invalid_glob_pattern_error), help("{0}"))]
+    #[diagnostic(help("{0}"))]
     WorldsDirectoryNotFound(io::ErrorKind, Vec<String>),
 
     #[error("cannot find the world `{1}` in local worlds directory")]
@@ -32,17 +32,14 @@ pub enum HazeError {
     WorldNotFound(Vec<PathBuf>, String),
 
     #[error("unable to find the local appdata directory")]
-    #[diagnostic(code(haze::world::local_appdata_error), help("{0}"))]
+    #[diagnostic(help("{0}"))]
     LocalAppData(io::Error),
 
     #[error("couldn't copy the world `{1}`")]
-    #[diagnostic(code(haze::world::copy_error), help("{0}"))]
+    #[diagnostic(help("{0}"))]
     WorldCopy(io::Error, ColoredString),
 
     #[error("cannot overwrite the world `{0}` as it already exists in `com.mojang`")]
-    #[diagnostic(
-        code(haze::world::cannot_overwrite_world),
-        help("do \"haze test --overwrite {0}\" if you want to overwrite")
-    )]
+    #[diagnostic(help("do \"haze test --overwrite {0}\" if you want to overwrite"))]
     OverwriteWorld(ColoredString),
 }
