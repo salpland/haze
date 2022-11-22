fc8efd3d801f75591abc07c96a306b2b49c9f99f
arexon <arexonreal@gmail.com>
2022-11-22 17:03:35 +0200

    refactor: main function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 use clap::Parser;
 use cli::{Cli, Command};
-use miette::Result;
 
 mod cli;
 mod config;
 mod error;
 mod world;
 
-fn main() -> Result<()> {
+fn main() -> miette::Result<()> {
     let cli = Cli::parse();
-
     let config = config::load(cli.config)?;
     match cli.command {
         Command::Test { name, overwrite } => world::test(name, config.worlds, overwrite)?,
         Command::Save { name } => world::save(name, config.worlds)?,
     }
-
     Ok(())
 }
